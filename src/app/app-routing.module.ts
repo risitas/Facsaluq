72af8d9dc71d5b5b553d2493b44798378fad02bc
Esteban Triviño <jorge.trivino@pysltda.com>
2021-10-27 10:40:16 -0500

    Prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path:'', redirectTo: 'login', pathMatch: "full"
+  },  {
+    path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
+
 ];
 @NgModule({
   imports: [
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from '../servicios/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +7,9 @@ import { AuthService } from '../servicios/auth.service';
 })
 export class LoginPage implements OnInit {
 
-  email: string;
-  password: string;
-
-  constructor(private authService: AuthService, public router: Router) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmitLogin() {
-    this.authService.login(this.email, this.password).then(res => {
-      //  this.router.navigate(['/entrada']);
-      alert('Ingreso correctamente');
-    }).catch(err => alert('los datos son incorrectos o el usuario no existe'));
-  }
 }
