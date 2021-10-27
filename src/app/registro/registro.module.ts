5997a889cb4283f90034739d549328a0f833bb1b
Esteban Triviño <jorge.trivino@pysltda.com>
2021-10-27 18:23:11 -0500

    Actualizacion

--- src/app/registro/registro.module.ts
+++ src/app/registro/registro.module.ts
@@ -4,31 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-<<<<<<< HEAD:src/app/registro/registro.module.ts
 import { RegistroPageRoutingModule } from './registro-routing.module';
 
 import { RegistroPage } from './registro.page';
-=======
-import { TableroMandoPageRoutingModule } from './tablero-mando-routing.module';
-
-import { TableroMandoPage } from './tablero-mando.page';
->>>>>>> master:src/app/tablero-mando/tablero-mando.module.ts
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-<<<<<<< HEAD:src/app/registro/registro.module.ts
     RegistroPageRoutingModule
   ],
   declarations: [RegistroPage]
 })
 export class RegistroPageModule {}
-=======
-    TableroMandoPageRoutingModule
-  ],
-  declarations: [TableroMandoPage]
-})
-export class TableroMandoPageModule {}
->>>>>>> master:src/app/tablero-mando/tablero-mando.module.ts
