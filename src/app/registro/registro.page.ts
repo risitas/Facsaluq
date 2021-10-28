b382a1b81ab2b16dc0c3cea60633041a87f5958a
Esteban Triviño <jorge.trivino@pysltda.com>
2021-10-28 16:57:32 -0500

    Mejoras

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../servicios/auth.service';
 import { Router } from '@angular/router';
 
 
+
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.page.html',
@@ -21,10 +22,25 @@ export class RegistroPage implements OnInit {
   ngOnInit() {
   }
 
+  
+
   onSubmitRegister() {
-    this.auth.register(this.email, this.password, this.nombres, this.apellidos, this.reppassword).then(auth => {
-      this.router.navigate(['/tableroMando']);
-      console.log(auth);
-    }).catch(err => console.log(err));
+
+    if(this.password===this.reppassword)
+    {
+      this.auth.register(this.email, this.password, this.nombres, this.apellidos).then(auth => {
+        this.router.navigate(['/tableroMando']);
+      }).catch(err => {
+            if(err.code==="auth/invalid-email"){
+              alert("El correo tiene un formato invalido.")
+            }else if(err.code==="auth/weak-password"){
+              alert("La contraseña debe tener minimo 6 caracteres.")
+            }else if(err.code==="auth/email-already-in-use"){
+              alert("Ya se encuentra un usuario registrado con ese correo.")
+            }
+        });
+    }else{
+      alert("Las constraseñas no coinciden")
+    }
   }
 }
